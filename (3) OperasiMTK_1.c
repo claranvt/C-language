6f648c2fcca1b2ba59e3a3155be76fb27af3f330
Clara <162611244+claranvt@users.noreply.github.com>
2024-03-08 00:29:44 +0700

    Update (3) OperasiMTK_1.c

--- (3) OperasiMTK_1.c
+++ (3) OperasiMTK_1.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+int main(){
+   int p,l,ht; // panjang, lebar, harga total
+   float L,H; // Luas Tanah, Harga per m^2
 
+   printf("Harga Tanah\t: ");
+   scanf("%d",&ht);
+   printt("Panjang\t: ");
+   scanf("%d",&p);
+   printt("Lebar\t: ");
+   scanf("%d",&l);
+  
+   L = p*l;
+   printf("\nLuas Tanah\t: %f m^2\n", L);
+   H = 
+  
+
+   
+   
+}
