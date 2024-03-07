08124f3a657ad76d15d517f533ff1ed2a7001ed0
Clara <162611244+claranvt@users.noreply.github.com>
2024-03-08 00:13:35 +0700

    Update (2) TipeDataBentukan_1.c

--- (2) TipeDataBentukan_1.c
+++ (2) TipeDataBentukan_1.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+struct data{
+      char nama[50],
+           jurusan[50],
+           email[50],
+           password[15];   
+}d;
 
+int main(){
+    printf("Sign In");
+    printf("Email\t: ");
+    gets(d.email);
+    printf("Password\t: ");
+    gets(d.password);
+    
+    
+}
